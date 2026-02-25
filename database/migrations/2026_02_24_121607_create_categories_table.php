8f1aa570d35589924f58f4db3f655898d40b31d1
IUB_LOKI <ayoubouharda33@gmail.com>
2026-02-25 09:40:24 +0000

    mon travail

--- database/migrations/2026_02_24_121607_create_categories_table.php
+++ database/migrations/2026_02_24_121607_create_categories_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table -> foreignId('colo')
             $table -> string('name') -> unique();
             $table->timestamps();
         });
